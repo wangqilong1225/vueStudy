50a43833bc9215b753b4135f6a262a4315e59755
wangqilong <664660754@qq.com>
2019-01-08 11:32:25 +0800

    update

---
+++ 4.基础_3.0/vue-playlist/src/components/Head.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="Head">
+    <h1>{{ Title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Head',
+  data () {
+    return {
+      Title: 'This is Head Scope'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.Head{
+    background-color:#333;
+    padding:0;
+    margin:0;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
