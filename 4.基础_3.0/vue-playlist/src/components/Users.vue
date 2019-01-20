2cd419e0601ecdba3d0d6371159790d436396a41
wangqilong <664660754@qq.com>
2019-01-20 17:06:52 +0800

    update

---
+++ 4.基础_3.0/vue-playlist/src/components/Users.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="users">
+    <ul>
+        <li v-for="user in users">
+          {{user}}
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'users',
+  data () {
+    return {
+     users:["Henry","Bucky","Emily"]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
